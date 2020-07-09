8b1a70e8193e849c34975698fe58dbe2facd149c
Carlos Max <carlosmax.dev@hotmail.com>
2020-07-09 21:23:02 +0100

    refactor: clean code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,14 +5,6 @@ import { Controller } from '../protocols/controller';
 
 export class SignUpController implements Controller {
   handle(_httpRequest: HttpRequest): HttpResponse {
-    if (!_httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'));
-    }
-
-    if (!_httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'));
-    }
-
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
     for (const field of requiredFields) {
       if (!_httpRequest.body[field]) {
