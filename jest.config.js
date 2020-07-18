c62dfb0306d9661de2a4a149cf9547dbadc65938
Carlos Max <carlosmax.dev@hotmail.com>
2020-07-18 19:31:25 +0100

    chore: test configuration

--- jest.config.js
+++ jest.config.js
@@ -7,13 +7,5 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['./src/**/*.ts'],
   coverageReporters: ['text', 'html'],
-  coverageDirectory: './coverage',
-  coverageThreshold: {
-    global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
-    },
-  },
+  coverageDirectory: './coverage'
 };
