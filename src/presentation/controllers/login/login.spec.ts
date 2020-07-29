930aba71ba89efd80e292a71a84aed7dbf1f7d58
Carlos Max <carlosmax.dev@hotmail.com>
2020-07-29 20:02:05 +0100

    test: ensure LoginController call authentication with correct values

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,19 +2,23 @@ import { LoginController } from './login';
 import { badRequest, serverError } from '../../helpers/http-helper';
 import { MissingParamError, InvalidParamError } from '../../erros';
 import { EmailValidator, HttpRequest } from '../signup/signup-protocols';
+import { Authentication } from '../../../domain/usecases/authentication';
 
 interface SutTypes {
   sut: LoginController;
   emailValidatorStub: EmailValidator;
+  authenticationStub: Authentication;
 }
 
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator();
-  const sut = new LoginController(emailValidatorStub);
+  const authenticationStub = makeAuthentication();
+  const sut = new LoginController(emailValidatorStub, authenticationStub);
 
   return {
     sut,
     emailValidatorStub,
+    authenticationStub
   };
 };
 
@@ -26,6 +30,16 @@ const makeEmailValidator = (): EmailValidator => {
   }
 
   return new EmailValidatorStub();
+};
+
+const makeAuthentication = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    async auth(email: string, password: string): Promise<string> {
+      return new Promise((resolve) => resolve('any_token'));
+    }
+  }
+
+  return new AuthenticationStub();
 };
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -79,5 +93,12 @@ describe('Login Controller', () => {
     });
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test('Should call Authentication with correct email', async () => {
+    const { sut, authenticationStub } = makeSut();
+    const authSpy = jest.spyOn(authenticationStub, 'auth');
+    await sut.handle(makeFakeRequest());
+    expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password');
   });
 });
