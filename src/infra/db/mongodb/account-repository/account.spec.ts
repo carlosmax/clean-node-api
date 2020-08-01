579489b8628546588a230afc79939fa2aba88df4
Carlos Max <carlosmax.dev@hotmail.com>
2020-08-01 21:46:00 +0100

    test: ensure AccountMongoRepository updates the accessToken on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,18 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com');
     expect(account).toBeFalsy();
   });
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut();
+    const result = await this.accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    expect(result.ops[0].accessToken).toBeFalsy();
+    await sut.updateAccessToken(result.ops[0]._id, 'any_token');
+    const account = await this.accountCollection.findOne({ _id: result.ops[0]._id });
+    expect(account).toBeTruthy();
+    expect(account.accessToken).toBe('any_token');
+  });
 });
