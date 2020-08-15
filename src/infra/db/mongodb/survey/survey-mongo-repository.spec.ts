cfc4698948df3d2e1ffb6091bf2c4e63ab18beed
Carlos Max <carlosmax.dev@hotmail.com>
2020-08-15 21:09:29 +0100

    test: ensure SurveyMongoRepository add a new survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,39 @@
+/* eslint-disable no-underscore-dangle */
+import { Collection } from 'mongodb';
+import { MongoHelper } from '../helpers/mongo-helper';
+import { SurveyMongoRepository } from './survey-mongo-repository';
+
+const makeSut = (): SurveyMongoRepository => new SurveyMongoRepository();
+
+describe('Survey Mongo Repository', () => {
+  let surveyCollection: Collection;
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany({});
+  });
+
+  test('Should add a Survey with success', async () => {
+    const sut = makeSut();
+    await sut.add({
+      question: 'any_question',
+      answers: [{
+        image: 'any_image',
+        answer: 'any_answer'
+      }, {
+        answer: 'other_answer'
+      }]
+    });
+
+    const survey = await surveyCollection.findOne({ question: 'any_question' });
+    expect(survey).toBeTruthy();
+  });
+});
