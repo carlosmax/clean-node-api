23d6f39c17f62c080f5351a486f3a5cb63c49239
Carlos Max <carlosmax.dev@hotmail.com>
2020-08-15 21:35:08 +0100

    feat: add AddSurvey composition

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -7,7 +7,7 @@ export const adaptRoute = (controller: Controller) => async (req: Request, res: 
   };
   const httpResponse = await controller.handle(httpRequest);
 
-  if (httpResponse.statusCode === 200) {
+  if (httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299) {
     res.status(httpResponse.statusCode).json(httpResponse.body);
   } else {
     res.status(httpResponse.statusCode).json({
