815c3a3a388487d31ef386a838d076b72a058f3f
Carlos Max <carlosmax.dev@hotmail.com>
2020-08-20 21:43:36 +0100

    test: ensure add survey route returns 204 if a valid accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,11 +1,16 @@
+/* eslint-disable no-underscore-dangle */
 import request from 'supertest';
 import { Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app';
+import env from '../config/env';
 
 let surveyCollection: Collection;
 
 describe('SignUp Routes', () => {
+  let accountCollection: Collection;
+
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -17,6 +22,9 @@ describe('SignUp Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.deleteMany({});
+
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
   });
 
   describe('POST /surveys', () => {
@@ -34,5 +42,39 @@ describe('SignUp Routes', () => {
         })
         .expect(403);
     });
+  });
+
+  test('Should return 204 on add survey with valid accessToken', async () => {
+    const res = await accountCollection.insertOne({
+      name: 'Carlos Max',
+      email: 'carlosmax.dev@hotmail.com',
+      password: '123',
+      role: 'admin'
+    });
+
+    const id = res.ops[0]._id;
+    const accessToken = sign({ id }, env.jwtSecret);
+
+    await accountCollection.updateOne({
+      _id: id
+    }, {
+      $set: {
+        accessToken
+      }
+    });
+
+    await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }]
+      })
+      .expect(204);
   });
 });
